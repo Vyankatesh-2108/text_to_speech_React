018d2740a8b36a6f0e0851f6653c4b760f95b871
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2024-02-13 12:39:07 +0530

    Text To Speech Converter using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSpeechSynthesis } from 'react-speech-kit'
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('')
+  const { speak, cancel } = useSpeechSynthesis();
+
+  const handleOnClick = () => {
+    speak({ text: text });
+  };
+
+  const handleStopClick = () => {
+    cancel();
+  };
+
+  const handleClearClick = () => {
+    setText('')
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='header'>Text To Speech Converter</h1>
+      <textarea className="textAreaStyle" placeholder='Type Here..!' value={text} onChange={(e) => { setText(e.target.value) }}></textarea>
+      <button className="buttonStyle" onClick={handleOnClick}>Listen</button>
+      <button className="buttonStyle" onClick={handleStopClick}>Stop</button>
+      <button className="buttonStyle" onClick={handleClearClick}>Clear Text</button>
     </div>
   );
 }
